68b492e13abb973b5cb2d59c3d90a57be0f22004
Sanket Manmode <manmodesanket@gmail.com>
2020-12-02 07:09:22 +0530

    fetch done

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ var textInput = document.querySelector("#txt-input");
 
 var textOutput = document.querySelector("#txt-output");
 
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
 btnTranslate.addEventListener("click", () => {
-    textOutput.value = textInput.value; 
+    var url2 = url + "?text=" + textInput.value;
+    console.log(url2); 
+    fetch(url2).then((res) => res.json()).then((res) => {
+        console.log(res);
+        textOutput.value = res.contents.text;
+    }) 
 })
