b0960815bd0f6c399e7025f901e37cfc93869b95
Sanket Manmode <manmodesanket@gmail.com>
2020-12-05 14:51:15 +0530

    changes in cs and footer added

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-console.log("hello");
-
 var btnTranslate = document.querySelector("#btn-translate");
 
 var textInput = document.querySelector("#txt-input");
 
 var textOutput = document.querySelector("#txt-output");
 
-var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var url = "https://api.funtranslations.com/translate/minion.json";
+
+var errorHandler = (err) => {
+    console.log("ERROR OCCURED");
+    alert("Error " + err);
+}
 
 btnTranslate.addEventListener("click", () => {
     var url2 = url + "?text=" + textInput.value;
     console.log(url2); 
     fetch(url2).then((res) => res.json()).then((res) => {
         console.log(res);
-        textOutput.value = res.contents.text;
-    }) 
+        textOutput.value = res.contents.translated;
+    })
+    .catch(errorHandler)
 })
